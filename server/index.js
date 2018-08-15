65df617f6bd0ac05d2f4305aa9d3a73393205d68
K. Eaglin <kceaglin@gmail.com>
2018-08-15 04:40:05 -0400

    server up; serving json

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const parser = require('body-parser')
 const app = express()
 const cors = require('cors')
+const data = require('../data/data')
 
 app.set('port', process.env.PORT || 3001)
 app.use(cors())
 app.use(parser.urlencoded({extended: true}))
 app.use(parser.json({extended: false}))
+
+app.get('/', (req, res) => {
+  res.json(data)
+})
 
 app.listen(app.get('port'), () => {
   console.log('It\'s aliiive! on PORT', app.get('port'))
