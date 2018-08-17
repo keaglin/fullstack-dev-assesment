0a6895cde586d2f84add36b3faa36edad18802a3
K. Eaglin <kceaglin@gmail.com>
2018-08-16 23:01:25 -0400

    GET req up and running; data being updated in state

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
+import { CLIENT_URL } from '../../helpers/constants'
 
 class App extends Component {
-  state
+  state = {
+    ads: []
+  }
+
+  componentDidMount() {
+    axios.get(CLIENT_URL)
+      .then(res => this.setState({ads: res.data.ads}))
+  }
   render() {
+    let ads = this.state.ads
+    console.log(ads)
+    // ads.forEach(ad => console.log(ad))
     return (
       <div className="App">
-        app
+        
       </div>
     );
   }
