38b93d6a6f3094cca45f26b0af003d33375218a6
K. Eaglin <kceaglin@gmail.com>
2018-08-16 21:50:43 -0400

    ad model

--- server/db/models/ad.js
+++ server/db/models/ad.js
@@ -1,6 +1,56 @@
 const mongoose = require('mongoose')
 
+const TargetSchema = new mongoose.Schema({
+  languages: Array,
+  genders: Array,
+  locations: Array,
+  interests: Array,
+  KeyWords: Array
+})
+
+const CreativeSchema = new mongoose.Schema({
+  header: String,
+  description: String,
+  url: String,
+  image: String
+})
+
+const InsightSchema = new mongoose.Schema({
+  impressions: Number,
+  clicks: Number,
+  nanos_score: Number,
+  cost_per_click: Number,
+  click_through_rate: Number,
+  advanced_kpi_1: Number,
+  advanced_kpi_2: Number
+})
+
+const SocialSchema = new mongoose.Schema({
+  status: String,
+  total_budget: Number,
+  remaining_budget: Number,
+  start_date: { type: Date, default: Date.now },
+  end_date: { type: Date, default: Date.now },
+  target_audience: TargetSchema,
+  creatives: CreativeSchema,
+  insights: InsightSchema
+})
+
+// const GoogleSchema = SocialSchema.extend({ KeyWords: Array })
+// if it doesn't work by just adding KeyWords but not using it, I'll use this
+
+const PlatformSchema = new mongoose.Schema({
+  facebook: SocialSchema,
+  instagram: SocialSchema,
+  google: SocialSchema
+})
+
 const AdSchema = new mongoose.Schema({
+  name: String,
+  goal: String,
+  total_budget: Number,
+  status: String,
+  platforms: PlatformSchema
 })
 
 mongoose.model('Ad', AdSchema)
