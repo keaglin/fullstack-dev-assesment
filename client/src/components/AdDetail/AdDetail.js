5bbae2985a31aa12bcb15c49259eed7b92e63fd8
K. Eaglin <kceaglin@gmail.com>
2018-08-22 03:36:44 -0400

    all data being rendered! from the top level to the deepest nested objs; todo: prettify?

--- client/src/components/AdDetail/AdDetail.js
+++ client/src/components/AdDetail/AdDetail.js
@@ -18,11 +18,18 @@ class AdDetail extends Component {
     this.state.ad ? ad = this.state.ad : this.getAd()
     let adProps = []
     for (let prop in ad) {
-      console.log('typeof ad[prop] is', typeof(ad[prop]))
+      // console.log('typeof ad[prop] is', typeof(ad[prop]))
       if (typeof(ad[prop]) !== 'object') {
         adProps.push([prop, ad[prop]])
       }
     }
+    // console.log('adProps array is', adProps)
+    // need to stub this logic out to a function to be used on
+    // all the Object-typed pieces of data
+    // function should take the object as an argument
+    // it also needs to know how to deal with arrays within 
+    // the given object
+    // use Object.entries() on all the objects
     adProps.shift()
 
     return (
--- client/src/components/PlatformContainer/PlatformContainer.js
+++ client/src/components/PlatformContainer/PlatformContainer.js
@@ -1,20 +1,36 @@
 import React from 'react'
-import PlatformDetail from '../PlatformDetail/PlatformDetail'
+import PlatformList from '../PlatformList/PlatformList'
 
 const PlatformContainer = (props) => {
-  // let platforms = props.platforms
-  let platforms = Object.entries(props.platforms)
-  console.log('platforms is', platforms)
+  let platforms = props.platforms
+  // let platforms = Object.entries(props.platforms)
+  platforms = Object.entries(platforms)
+  // console.log('platforms is', platforms)
   platforms.shift()
-  console.log('platforms after shift is', platforms)
+  // console.log('platforms after shift is', platforms)
   // console.log(Object.keys(platforms))
   // console.log(Object.entries(platforms).shift())
+  // const objToArr = (obj) => {
+  //   return Object.entries(obj)
+  // }
+  let platProps = []
+    for (let prop in platforms) {
+      // console.log('typeof ad[prop] is', typeof(ad[prop]))
+      if (typeof(platforms[prop]) !== 'object') {
+        platProps.push([prop, platforms[prop]])
+      }
+    }
+  // adProps.shift()
   return (
     <div>
+      <p>Platforms:</p>
       {
         platforms.map(plat => {
           return (
-            <li><PlatformDetail name={plat[0]} data={plat[1]}/>
+            <li>
+              {plat[0]}:
+              {/* <PlatformList name={plat[0]} data={plat[1]}/> */}
+              <PlatformList data={plat}/>
             </li>
           )
         })
