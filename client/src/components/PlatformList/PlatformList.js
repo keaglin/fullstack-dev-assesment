e537cd72eeb50968d7d601b21ad357d5e408b5c3
K. Eaglin <kceaglin@gmail.com>
2018-08-22 05:34:36 -0400

    fixed images; they show up now instead of text and the other info shows as well

--- client/src/components/PlatformList/PlatformList.js
+++ client/src/components/PlatformList/PlatformList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './PlatformList.css'
 
 const PlatformList = (props) => {
   let platform = props.data
@@ -6,18 +7,34 @@ const PlatformList = (props) => {
   platformValue.shift()
   const displayCollection = (el) => {
     let propString = ''
-    console.log('from displayCollection, the element we on is', el)
-    Object.entries(el).map(prop => {
-      propString = prop.join(': ')
-      console.log('propString is', propString)
+    return Object.entries(el).map(prop => {
+      console.log('prop[0] is', prop[0])
+      console.log(`prop[0] === 'image' is`, prop[0] === 'image')
+      if (prop[0] === 'image') {
+        return (
+          // `THIS IS A TEST`
+          <div>
+            {/* TEST */}
+            <ul>
+              <li>image: <img src={require(`../../../public/images/${prop[1]}`)} alt={prop[1]} /></li>
+            </ul>
+          </div>
+        )
+      }
+      else {
+        console.log('img condition failed')
+        propString = prop.join(': ')
+        console.log(propString)
+        return (
+          <div>
+            <ul>
+              <li>{propString}</li>
+            </ul>
+          </div>
+        )
+      }
     })
-    return (
-      <div>
-        <ul>
-          <li>{propString}</li>
-        </ul>
-      </div>
-    )
+    
   }
 
   
