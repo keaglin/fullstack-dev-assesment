18118e0a982fdb5a14636b38d30818a383785f0f
K. Eaglin <kceaglin@gmail.com>
2018-08-22 04:05:37 -0400

    updated readme and added necessary files and changes to prep for deployment to Heroku

--- server/db/connection.js
+++ server/db/connection.js
@@ -3,10 +3,15 @@ const mongoose = require('./models/ad')
 mongoose.Promise = Promise
 
 const mongoUri = 'mongodb://localhost:27017/nanos'
-
-mongoose
-  .connect(mongoUri, { useNewUrlParser: true })
-  .then(connection => console.log(`Connection established to dev db '${connection.db.databaseName}'`))
-  .catch(connectionError => console.log('Connection to dev db failed!', connectionError))
-
+if (process.env.NODE_ENV == "production") {
+  mongoose
+    .connect(process.env.MLAB_URL)
+    .then(connection => console.log(`Connection established to production db '${connection.db.databaseName}'`))
+    .catch(connectionError => console.log('Connection to prod db failed!', connectionError))
+} else {
+  mongoose
+    .connect(mongoUri, { useNewUrlParser: true })
+    .then(connection => console.log(`Connection established to dev db '${connection.db.databaseName}'`))
+    .catch(connectionError => console.log('Connection to dev db failed!', connectionError))
+}
 module.exports = mongoose
